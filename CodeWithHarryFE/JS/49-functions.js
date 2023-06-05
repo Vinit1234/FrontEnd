6cf85f0cbb7a5e1f12b7d02b6c8873b86f29fb37
Vinit1234 <vinitsakhare5678@gmail.com>
2023-06-05 01:36:45 -0700

    Functions

---
+++ CodeWithHarryFE/JS/49-functions.js
@@ -0,0 +1,22 @@
+// Default value of greetText="Greetings!"
+function greet(name, greetText="Greetings!"){
+    let name1="Zeus";
+    console.log(greetText+" "+name);
+    console.log("Inside greet() name1 is "+name1)
+  }
+
+function sum(a,b,c){
+    return a+b+c;
+}
+
+  let name="John";
+  greet(name,"Hello"); 
+  greet(name,"Good morning!"); 
+  greet("Mohan");
+  let name1="Chhotu";
+  console.log("Here name1 is "+name1);
+
+let d = sum(10,20,30)
+console.log(d);
+
+
