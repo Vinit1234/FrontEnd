a5239750b0a1cc881ed7b022fd71675f613d51a6
Vinit Sakhare <vinitsakhare@gmail.com>
2024-08-27 11:08:07 -0700

    Added more logic to TextUtils

--- CodeWithHarryFE/React/codes/my-app/src/App.js
+++ CodeWithHarryFE/React/codes/my-app/src/App.js
@@ -31,54 +31,71 @@ function App() {
     //     <h1>Hello {name}</h1>
     //   </div>
     // </>
-    
+
     // ** Example-2=======================================
     // Copy pasted Navbar code from Bootstrap and modified
-    // Tip: 1. find open tags and close all tags 
+    // Tip: 1. find open tags and close all tags
     // 2. replace class by className
     // 3. replce href="/" to href="/"
     // 4. add href="/" to wherever it is missing in anchor tag
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-<hr />
-  {/* <Navbar/> */}
-  
-  {/* Component with props */}
-  {/* <Navbar title="TextUtils2" aboutText="About-TextUtils"/> */}
+      {/* <nav className="navbar navbar-expand-lg bg-body-tertiary"> */}
+      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            TextUtils
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <hr />
+      {/* <Navbar/> */}
 
-  <Navbar title="TextUtils"/>
-  
-  {/* For explaining states and event handling  */}
-  <div className="container my-3">
-    <TextForm heading="Enter a text to analyse."/>
-  </div>
+      {/* Component with props */}
+      {/* <Navbar title="TextUtils2" aboutText="About-TextUtils"/> */}
 
+      <Navbar title="TextUtils" />
 
-
-
+      {/* For explaining states and event handling  */}
+      <div className="container my-3">
+        <TextForm heading="Enter a text to analyse." />
+      </div>
     </>
-
   );
 }
 
--- CodeWithHarryFE/React/codes/my-app/src/component/TextForm.js
+++ CodeWithHarryFE/React/codes/my-app/src/component/TextForm.js
@@ -3,7 +3,9 @@ import React, {useState} from "react";
 
 export default function TextForm(props) {
     // hooks
-    const [text, setText] = useState("Enter text here");
+    // const [text, setText] = useState("Enter text here");
+
+    const [text, setText] = useState("");
     // Calling setText inside this function puts React into infinite loop
     // and breaks the application. 
     // setText("Some new text");  //Illegal
@@ -23,7 +25,13 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleLoClick=()=>{
+      let newText = text.toLowerCase();
+      setText(newText);
+  }
+
     return (
+      <>
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
@@ -41,7 +49,17 @@ export default function TextForm(props) {
           onChange={handleOnChange}
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} words, {text.length} letters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
